dbd16317008e231c6cba8b44cf95c3c48927e964
Santiago Perez Perret <perezperret@gmail.com>
2020-08-11 18:56:48 -0300

    Broadcast first state change on `ProcessRunJob`

--- app/jobs/process_run_job.rb
+++ app/jobs/process_run_job.rb
@@ -3,8 +3,9 @@ class ProcessRunJob < ApplicationJob
 
   def perform(process_run)
     process_run.start
-    process_run.run
+    ProcessRunsChannel.broadcast_to process_run, status: process_run.status
 
+    process_run.run
     ProcessRunsChannel.broadcast_to process_run, status: process_run.status
   end
 end
--- spec/jobs/process_run_job_spec.rb
+++ spec/jobs/process_run_job_spec.rb
@@ -8,13 +8,14 @@ RSpec.describe ProcessRunJob, type: :job do
     expect(process_run.reload.status).to eq('completed').or(eq('failed'))
   end
 
-  it 'broadcasts `ProcessRun#status` on completion' do
+  it 'broadcasts `ProcessRun#status` on each step' do
     expect do
       described_class.perform_now(process_run)
     end.to(
       have_broadcasted_to(ProcessRunsChannel.broadcasting_for(process_run))
+        .exactly(2).times
         .with do |data|
-          expect(data).to eq('status' => process_run.status)
+          expect(data['status']).to match(/completed|failed|started/)
         end
     )
   end
